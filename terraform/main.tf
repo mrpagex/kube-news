ba4d6e6da8d947d75b412ee182816cf7070b116d
mrpagex <esdras.page@gmail.com>
2023-04-14 08:16:57 -0300

    Correção versão

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,8 +24,8 @@ data "digitalocean_ssh_key" "ssh_key" {
   name = var.ssh_key_name
 }
 
-resource "digitalocean_kubernetes_cluster" "k8ss" {
-  name   = "k8ss"
+resource "digitalocean_kubernetes_cluster" "k8s" {
+  name   = "k8s"
   region = var.region
   # Grab the latest version slug from `doctl kubernetes options versions`
   version = "1.26.3-do.0"
@@ -53,7 +53,7 @@ output "jenkins_ip" {
   value = digitalocean_droplet.jenkins.ipv4_address
 }
 
-resource "local_file" "foo" {
-  content  = digitalocean_kubernetes_cluster.k8ss.kube_config.0.raw_config
+resource "local_file" "kube_config" {
+  content  = digitalocean_kubernetes_cluster.k8s.kube_config.0.raw_config
   filename = "kube_config.yaml"
 }
